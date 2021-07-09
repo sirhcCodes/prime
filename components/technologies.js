fafce32fb15cf719aab1df2ce696e3fab9cb55f4
IDTaken8 <cawong86@gmail.com>
2021-07-08 22:42:01 -0400

    Added GitHub icon & Styling update

--- components/technologies.js
+++ components/technologies.js
@@ -5,32 +5,43 @@ import {
   SiJavascript,
   SiReact,
   SiNextDotJs,
+  SiGithub,
 } from 'react-icons/si';
 
 const Technologies = () => {
-  const technologyStyles = ' h-12 w-12 text-gray-600';
+  const technologyStyles = 'block h-12 w-12 text-gray-600 mx-auto';
 
   return (
     <div>
       <h3 className='text-center text-2xl font-bold mb-4'>Technologies</h3>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-items-center	gap-8 mb-8 text-center'>
+      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-7 justify-items-center gap-x-16 gap-y-8 mb-8 '>
         <div>
           <SiHtml5 className={technologyStyles} />
+          <p>HTML</p>
         </div>
         <div>
           <SiCss3 className={technologyStyles} />
+          <p>CSS</p>
         </div>
         <div>
           <SiTailwindcss className={technologyStyles} />
+          <p>TailwindCSS</p>
         </div>
         <div>
           <SiJavascript className={technologyStyles} />
+          <p>JS</p>
         </div>
         <div>
           <SiReact className={technologyStyles} />
+          <p>React</p>
         </div>
         <div>
           <SiNextDotJs className={technologyStyles} />
+          <p>Next.js</p>
+        </div>
+        <div>
+          <SiGithub className={technologyStyles} />
+          <p>GitHub</p>
         </div>
       </div>
     </div>
